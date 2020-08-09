80732e35eee3c2215a05086fa4e2a03c7c5bcea1
Aglayrton <aglayrtonjuliao@gmail.com>
2020-08-09 00:32:00 -0300

    Insert funcionando via Test pela classe CrudRepository

--- src/test/java/projeto/spring/data/treinamento/AppSprintDataTest.java
+++ src/test/java/projeto/spring/data/treinamento/AppSprintDataTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.treinamento.dao.InterfaceSpringDataUser;
+import projeto.spring.data.treinamento.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})//busca o caminho do projeto
@@ -17,6 +18,17 @@ public class AppSprintDataTest {
 	
 	@Test
 	public void testeInsert() {
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setEmail("aglayrtonjuliao@gmail.com");
+		usuarioSpringData.setNome("Aglayrton");
+		usuarioSpringData.setLogin("admin");
+		usuarioSpringData.setSenha("admin");
+		
+		interfaceSpringDataUser.save(usuarioSpringData);
+	}
+	
+	@Test
+	public void testeConsulta() {
 		System.out.println("Iniciou o spring com sucesso");
 	}
 	
