e4b526793c4671c03a9915bc7c7a3b4c81a5d5b1
Aglayrton <aglayrtonjuliao@gmail.com>
2020-08-09 00:14:22 -0300

    Injetado a dependencia da interface do dao

--- src/test/java/projeto/spring/data/treinamento/AppSprintDataTest.java
+++ src/test/java/projeto/spring/data/treinamento/AppSprintDataTest.java
@@ -2,12 +2,18 @@ package projeto.spring.data.treinamento;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.treinamento.dao.InterfaceSpringDataUser;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})//busca o caminho do projeto
 public class AppSprintDataTest {
+	
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringDataUser;
 	
 	@Test
 	public void testeInsert() {
