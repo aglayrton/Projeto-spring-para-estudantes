1d1d69806dc9c9d8a227f11b34a3f64faab52843
Aglayrton <aglayrtonjuliao@gmail.com>
2020-08-19 10:19:15 -0300

    Criado a Entidade e a interface para injenção de depência na classe de Teste

---
+++ src/main/java/projeto/spring/data/treinamento/dao/InterfaceSpringDataTelefone.java
@@ -0,0 +1,9 @@
+package projeto.spring.data.treinamento.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import projeto.spring.data.treinamento.model.Telefone;
+
+public interface InterfaceSpringDataTelefone extends CrudRepository<Telefone, Long>{
+
+}
