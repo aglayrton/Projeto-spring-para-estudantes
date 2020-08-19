b325a5d69a859eeac02fab3265e39f1be6e62944
Aglayrton <aglayrtonjuliao@gmail.com>
2020-08-18 22:10:37 -0300

    Foi criado três métodos, dois de consultas por nome usando paramêtro e outro por argumento simples, além disso um método para processar qualquer dado a ser salvo

--- src/main/java/projeto/spring/data/treinamento/dao/InterfaceSpringDataUser.java
+++ src/main/java/projeto/spring/data/treinamento/dao/InterfaceSpringDataUser.java
@@ -1,11 +1,31 @@
 package projeto.spring.data.treinamento.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import projeto.spring.data.treinamento.model.UsuarioSpringData;
 
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
+	
+	//p de pessoa da classe
+	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
+	public List<UsuarioSpringData> buscaPorNome(String nome);
+	
+	@Query(value = "select p from UsuarioSpringData p where p.nome = :paranome")
+	public UsuarioSpringData buscarPorNomeParam(@Param("paranome") String paranome);
+	
+	
+	default <S extends UsuarioSpringData> S saveAtual(S entity) {
+		//processa qualquer coisa
+		return save(entity);
+	}
+	
+	
+	
 	
 }
